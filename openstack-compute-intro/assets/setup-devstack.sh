7be4d2cecdce65aca86e4a4dd1f1d62b51625b40
Lemontea <lemontea.Tom@gmail.com>
2022-05-14 01:51:34 +0800

    [minor] even stranger - why is file required on both root and assets/

---
+++ openstack-compute-intro/assets/setup-devstack.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+mkswap /swap.img && swapon /swap.img
+
+apt-get update
+DEBIAN_FRONTEND=noninteractive \
+  apt-get \
+  -o Dpkg::Options::=--force-confold \
+  -o Dpkg::Options::=--force-confdef \
+  -y --allow-downgrades --allow-remove-essential --allow-change-held-packages \
+  upgrade || true
+apt-get -y dist-upgrade || true
+
+sudo useradd -s /bin/bash -d /opt/stack -m stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
+
+cp ~/tcat.sh /opt/stack/
+cp ~/local.conf /opt/stack/
+chown stack /opt/stack/*
+chmod 755 /opt/stack/tcat.sh
+
+su - stack
+git clone https://git.openstack.org/openstack-dev/devstack
+
+export LOCAL_IP=`ip -j -4 a show ens3 | jq .[].addr_info[].local`
+
+./tcat.sh local.conf > ./devstack/local.conf
+
+cd devstack/ && ./stack.sh
